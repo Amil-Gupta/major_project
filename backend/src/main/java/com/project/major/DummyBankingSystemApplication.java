9d4ee3a8c22d248f7cc47d5a45fe02bae728ff82
Joy Patel <joypatel280@gmail.com>
2023-02-06 15:17:55 +0530

    1: Initial setup

--- backend/src/main/java/com/project/major/DummyBankingSystemApplication.java
+++ backend/src/main/java/com/project/major/DummyBankingSystemApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.externalpackage.test")
 public class DummyBankingSystemApplication {
 
 	public static void main(String[] args) {

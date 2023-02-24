34c47a057ecf8eb6c1746f57815809930ad964f7
Joy Patel <joypatel280@gmail.com>
2023-02-24 22:06:01 +0530

    #8: Add admin flag to AccountResource

--- backend/src/main/java/com/project/major/dto/AccountResource.java
+++ backend/src/main/java/com/project/major/dto/AccountResource.java
@@ -6,11 +6,12 @@ import lombok.Value;
 
 @Value
 public class AccountResource {
-	
+
 	Integer id;
 	Long balancePaise;
-	
+	boolean admin;
+
 	public static AccountResource of(Account account) {
-		return new AccountResource(account.getId(), account.getBalancePaise());
+		return new AccountResource(account.getId(), account.getBalancePaise(), account.isAdmin());
 	}
 }

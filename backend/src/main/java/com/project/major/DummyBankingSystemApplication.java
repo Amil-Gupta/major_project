fa35ba446e2bb7e2d4d8ee868521b4481cd97988
Amil Utkarsh Gupta <amiluguptaa@gmail.com>
2023-02-26 18:25:23 +0530

    added sts devtools dependency to pom.xml, solved CORS error for origin port 3000

--- backend/src/main/java/com/project/major/DummyBankingSystemApplication.java
+++ backend/src/main/java/com/project/major/DummyBankingSystemApplication.java
@@ -3,12 +3,25 @@ package com.project.major;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = { UserDetailsServiceAutoConfiguration.class })
 public class DummyBankingSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DummyBankingSystemApplication.class, args);
+	}
+	
+	@Bean
+	WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/*").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }

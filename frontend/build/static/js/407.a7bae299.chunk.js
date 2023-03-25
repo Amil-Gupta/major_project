dcb53cdad8a7ad44d8e746d472dfea7eb4b0505d
Amil Utkarsh Gupta <amiluguptaa@gmail.com>
2023-03-26 01:17:05 +0530

    created a production build

---
+++ frontend/build/static/js/407.a7bae299.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfrontend=self.webpackChunkfrontend||[]).push([[407],{7407:function(e,n,t){t.r(n);var r=t(3700),o=t(8381),i=t(7174),c=t(9343);n.default=function(){var e=(0,o.useContext)(r.Z).setAuth;return(0,o.useEffect)((function(){e({})}),[]),(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)("h1",{children:"Hi There! Kindly wait to be redirected to a better-looking page."}),(0,c.jsx)(i.OL,{to:"/login",replace:!0,children:"Take Me To One Now!"})]})}}}]);
+//# sourceMappingURL=407.a7bae299.chunk.js.map

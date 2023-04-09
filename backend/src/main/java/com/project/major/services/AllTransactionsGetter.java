c89a607335b8d5ac3e28da38197cfd266d650669
Joy Patel <joypatel280@gmail.com>
2023-04-09 13:14:16 +0530

    #16: Added Get all transactions API for admin.

---
+++ backend/src/main/java/com/project/major/services/AllTransactionsGetter.java
@@ -0,0 +1,39 @@
+package com.project.major.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+import com.project.major.dto.TransferResource;
+import com.project.major.error.BusinessException;
+import com.project.major.repositories.AccountRepository;
+import com.project.major.repositories.TransferRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AllTransactionsGetter {
+	
+	private final AccountRepository accountRepository;
+	private final TransferRepository transferRepository;
+	
+	
+	public List<TransferResource> get(Integer id) {
+		var account = accountRepository.findById(id).orElseThrow();
+		
+		if (!account.isAdmin())
+			throw new BusinessException("Only Admin can access this info!", HttpStatus.FORBIDDEN);
+		
+		log.info("Admin id: {} is fetching all transactions.", id);
+		
+		return transferRepository.findByOrderByTransferredAtAsc()
+				.stream()
+				.map(TransferResource::of)
+				.collect(Collectors.toList());
+	}
+}
--- backend/src/main/java/com/project/major/services/StatementGetter.java
+++ backend/src/main/java/com/project/major/services/StatementGetter.java
@@ -1,13 +1,11 @@
 package com.project.major.services;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.project.major.dto.StatementResource;
-import com.project.major.entities.Transfer;
 import com.project.major.repositories.AccountRepository;
 import com.project.major.repositories.TransferRepository;
 
@@ -24,11 +22,7 @@ public class StatementGetter {
 		var account = accountRepository.findById(accountId).orElseThrow();
 		var sortedPageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("transferredAt").descending());
 		
-		Page<Transfer> transfers;
-		if(account.isAdmin())
-			transfers = transferRepository.findAll(sortedPageable);
-		else 
-			transfers = transferRepository.findByFromAccountOrToAccount(account, account, sortedPageable);
+		var	transfers = transferRepository.findByFromAccountOrToAccount(account, account, sortedPageable);
 		
 		return StatementResource.of(account, transfers);
 	}

7d73c9cfad55e879ee4ae9a0ae8186050d2665e0
Amil Utkarsh Gupta <amiluguptaa@gmail.com>
2023-05-07 01:39:59 +0530

    removed some commented code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,6 @@ import { BrowserRouter } from 'react-router-dom';
 import AppProvider from 'context/AppProvider';
 import ErrorBoundaryFunctional from 'error/ErrorBoundaryFunctional';
 import ErrorPage from 'error/ErrorPage';
-// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -19,8 +18,3 @@ root.render(
     </AppProvider>
   </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
